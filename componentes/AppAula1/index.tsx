f9417d235502ed0a0cbe699fa35401bd78d7be38
Olavo Adriel <ollavoadriel@gmail.com>
2024-07-14 02:13:34 -0300

    Tela de login: Front

--- componentes/AppAula1/index.tsx
+++ componentes/AppAula1/index.tsx
@@ -1,11 +1,95 @@
-import {Text, ScrollView, View, Image, TouchableOpacity} from 'react-native'
+import {Text, ScrollView, View, Image, TextInput, TouchableOpacity, Alert} from 'react-native'
 import {styles} from './style'
+import React, {useState} from 'react';
+
+
+
 export const ScreenLogin = () =>{
+
+    const [emailField, setEmailField] = useState<string>('');
+    const [passwordField, setpasswordField] = useState<string>('');
+
+    function checkToNavigate(){
+        if((emailField, passwordField) != ''){
+            return handleLoginButton
+        }
+    } 
+    const handlePress = () => {
+        const navigate = checkToNavigate();
+        if (navigate) {
+          navigate(); // Chama a função handleLoginButton se a condição for atendida
+        } else {
+          Alert.alert("Erro", "Por favor, preencha todos os campos.");
+        }
+      };
+
+    const handleForgetButton = () =>{
+
+    }
+    const handleSignUpButton = () =>{
+
+    }
+    const handleLoginButton = () =>{
+        alert(emailField)
+        alert(passwordField)
+    }
+
     return(
-       <ScrollView>
-            <Text style={styles.ScrollView}></Text>
+       <ScrollView style={styles.ScrollView}>
             <View style={styles.container}>
-                <Image source={require('')}/>
+                <Image source={require('../../assets/img/screenLogin/Logo.png')} style={styles.logo}/>
+                <Text style={styles.h1}> Clean Hands</Text>
+                <Text style={styles.h2}> Seja bem vindo(a)! </Text>
+                <Text style={styles.h2}> Digite seus dados abaixo</Text>
+            
+                <View style={styles.areaTextInput}>
+                    <Text style={styles.inputLabel}> Email</Text>
+                    <TextInput
+                    style={styles.inputField}
+                    placeholder='Digite seu email!'
+                    placeholderTextColor="#999"
+                    value={emailField}
+                    onChangeText={t => setEmailField(t)}
+                    autoCapitalize='none'
+                    keyboardType='email-address'
+                    
+                    />
+
+                </View>
+                <View style={styles.areaTextInput}>
+                    <Text style={styles.inputLabel}> Senha</Text>
+                    <TextInput
+                    style={styles.inputField}
+                    placeholder='*******'
+                    secureTextEntry
+                    value={passwordField}
+                    onChangeText={t=> setpasswordField(t)}
+                    placeholderTextColor="#999"
+
+                    />
+
+                </View>
+
+                <View style={styles.aditionals}>
+                    <TouchableOpacity onPress={handleForgetButton} style={styles.forgotBtnArea}>
+                        <Text style={styles.forgotBtnText}>Esqueci minha senha</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <TouchableOpacity onPress={handlePress} style={styles.button}>
+                    <Text style={styles.buttonText}>Entrar</Text>
+                </TouchableOpacity>
+
+                <View style={styles.signUpArea}>
+                    <Text style={styles.signUpText}>Não tem cadastro?</Text>
+                    <TouchableOpacity onPress={handleSignUpButton}>
+                        <Text style={styles.signUpBtnText}>Cadastra-se</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.footerArea}>
+                    <Text style={styles.footerText}>Criado Por Olavo Adriel</Text>
+                </View>
+                
             </View>
        </ScrollView>
     );
--- componentes/AppAula1/style.tsx
+++ componentes/AppAula1/style.tsx
@@ -1,6 +1,97 @@
 import { StyleSheet } from "react-native"
 export const styles= StyleSheet.create({
+    h1:{
+        color:'#000',
+        fontSize:27,
+        fontWeight:'bold',
+        marginVertical:10
+    },
+    h2:{
+        color:'#999',
+        fontSize:15
+    },
     texto:{
         backgroundColor:'blue'
+    },
+    ScrollView:{
+        backgroundColor:'#ffffff',
+        paddingTop:40,
+        paddingHorizontal:15
+    },
+    container:{
+        alignItems:'center',
+
+    },
+    logo:{
+        width:150,
+        height:150
+    },
+    areaTextInput:{
+
+        width:'100%',
+        marginTop:20,
+
+    },
+    inputLabel:{
+        color:'#777',
+        fontSize:14,
+        fontWeight:'bold',
+        marginBottom:7
+    },
+    inputField:{
+        borderWidth:2,
+        borderRadius:5,
+        borderColor:'#000',
+        fontSize:15,
+        padding:10
+    },
+    aditionals:{
+        width:'100%',
+
+    },
+    forgotBtnArea:{
+        paddingVertical:20,
+        alignSelf:'flex-end'
+    },
+    forgotBtnText:{
+        fontSize:14,
+        fontWeight:'bold',
+        color:'green'
+    },
+    button:{
+        backgroundColor:'green',
+        width:'100%',
+        padding:10,
+        borderRadius:5
+    },
+    buttonText:{
+        alignSelf:'center',
+        fontSize:15,
+        color:'white'
+    },
+    signUpArea:{
+        flexDirection:'row',
+        marginTop:30
+    },
+    signUpText:{
+        fontSize:13,
+        fontWeight:'bold',
+        color:'#999',
+
+    },
+    signUpBtnText:{
+        fontSize:13,
+        fontWeight:'bold',
+        color:'green',
+        marginLeft:4
+    },
+    footerArea:{
+        marginVertical:30,
+
+    },
+    footerText:{
+        fontSize:13,
+        color:'green'
     }
+    
 });
