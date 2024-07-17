fef2f60e02f159f6d382826d94cbcc679d442ee8
Olavo Adriel <ollavoadriel@gmail.com>
2024-07-17 00:13:16 -0300

    Aplicativo de filmes populares e mais avaliados

--- componentes/FIlmesEmCartaz/index.tsx
+++ componentes/FIlmesEmCartaz/index.tsx
@@ -1,28 +1,65 @@
-import React, { useState } from 'react';
-import {Button, Text, View, SafeAreaView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, Text, View, Image, SafeAreaView, FlatList, ActivityIndicator, TouchableOpacity} from 'react-native';
+import Constants from 'expo-constants';
 import { styles } from './style';
+
 
 
 export const FilmesEmCartaz = () =>{
 
+    const statusBarHeight = Constants.statusBarHeight;
+    const [loading, setLoading] = useState(true);
     const  [movies, setMovies] = useState([]);
+    const [typeListMovies, setTypeListMovies ] = useState('popular');
 
-    const handleLoadButton = async () =>{
-
-
-        const req = await fetch("https://api.b7web.com.br/cinema/")
-        const json = await req.json();
-
-        if(json){
-            setMovies(json);
+    useEffect(()=>{
+        const requestMovies = async () => {
+            setLoading(true)
+            const req = await fetch(`https://api.themoviedb.org/3/movie/${typeListMovies}?language=pt-BR&api_key=e4c777a91f99e5aa2cbd14b2aedfad4c`)
+            const json = await req.json();
+            
+            if(json){
+                setMovies(json.results)
+            }
+            setLoading(false)
         }
+        requestMovies();
+    }, [typeListMovies])
 
-    }
+  
     
-    return(    
-    <SafeAreaView style={styles.container}>
-        <Button title="Carregar filmes" onPress={handleLoadButton} />
-        <Text>FIlmes: {movies.length}</Text>
+    return(   
+        <SafeAreaView style={[styles.container, {marginTop: statusBarHeight + 8}]}>
+            <View style={styles.typeListMoviesButtonView} >
+            <TouchableOpacity style={styles.typeListMoviesButton} onPress={() => setTypeListMovies('top_rated')}>
+                <Text>Melhor avaliado </Text>
+            
+            </TouchableOpacity> 
+            <TouchableOpacity style={styles.typeListMoviesButton} onPress={() => setTypeListMovies('popular')}>
+                <Text>Populares</Text>
+            
+            </TouchableOpacity> 
+            </View>
+           
+        { loading &&
+            <View style={styles.loadingArea}>
+                <ActivityIndicator size="large" color='#FFF'/>
+                <Text style={styles.loadingText}>Carregando</Text>
+            </View>
+        }
+        { !loading &&
+        <>
+            <Text style={styles.totalMoviesText}>FIlmes: {movies.length}</Text>
+            <FlatList style={styles.movieList} data={movies} renderItem={({item})=>(
+                <View style={styles.movieItem}>
+                    <Text style={styles.movieTitle}>{item.title}</Text>
+                    <Image resizeMode="contain" source={{uri: "https://image.tmdb.org/t/p/w500/" + item.poster_path}} style={styles.movieImage}/>
+                </View>
+            )}
+        keyExtractor={item =>  item.id.toString()}
+        />
+        </>
+    }
     </SafeAreaView>
 );
 } 
--- componentes/FIlmesEmCartaz/style.ts
+++ componentes/FIlmesEmCartaz/style.ts
@@ -1,8 +1,64 @@
 import React  from "react";
+
+
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container:{
-        marginTop:50
+        flex:1
+        ,backgroundColor:'#333'
+    },
+    typeListMoviesButtonView:{
+        flexDirection:'row',
+        justifyContent:'center'
+    },
+    typeListMoviesButton:{
+        height:45,
+        width:170,
+        backgroundColor:'#1b7ccc',
+        justifyContent:'center',
+        borderRadius:10,
+        alignItems:'center',
+        flexDirection:'row'
+    
+    },
+    loadingArea:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    loadingText:{
+        color:'#FFF',
+
+    },
+    totalMoviesText:{
+        color:'white',
+        fontSize:18,
+        textAlign:'center',
+        margin:10,
+        marginBottom:25
+        
+    },
+    movieList:{
+        flex:1,
+        
+    },
+    movieItem:{
+        alignItems:'center',
+        marginBottom:1,
+    },
+    movieTitle:{
+        fontSize:24,
+        color:'#FFF',
+        fontFamily: 'Roboto',
+        textAlign:'center',
+        marginTop:5,
+        marginBottom:1
+    },
+    movieImage:{
+        borderRadius:15,
+        height:400,
+        width:200
     }
+
 })
